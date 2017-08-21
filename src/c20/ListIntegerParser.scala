d0c4fb8e5bbc743adb06cf3eda1bbf49c0ee911f
Eugene Konopaka <ykonopaka@gmail.com>
2017-08-21 22:23:35 +0200

    ex 3

--- src/c20/ListIntegerParser.scala
+++ src/c20/ListIntegerParser.scala
@@ -1,21 +1,16 @@
 package c20
 
-import scala.collection.mutable.ArrayBuffer
+import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 
 class ListIntegerParser extends RegexParsers {
 
-  val n = "-?[0-9]+".r
+  val number: Regex = "-?[0-9]+".r
 
-  val list = new ArrayBuffer[Int]
-
-  def element: Parser[Int] = n ^^ {
-    _.toInt
-  }
+  def element: Parser[Int] = number ^^ { _.toInt }
 
   def sequence: Parser[List[Int]] = (element ~ rep("," ~ element)) ^^ {
     case e ~ n => e :: n.map(f => f._2)
     case e => List(e._1)
   }
-
 }
